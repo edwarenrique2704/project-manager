fb4e8ce77866cee78fd8e67739f0999633bc9b95
Edward Enrique Garcia Rizo <GNeegarcia@ads.loc>
2024-10-07 11:04:07 -0500

    add project

---
+++ backend/config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+
+  //nota : cambiando la bas por local , funciona
+
+  dbURL:'mongodb://127.0.0.1/',
+  //dbURL: 'mongodb+srv://idandavid:idan5375@cluster0.zkktgr5.mongodb.net/?retryWrites=true&w=majority',
+  dbName: 'monday_DB'
+}
